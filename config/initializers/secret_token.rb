e7633461169bf5af2af002a9611c203b7ce29936
OGRE7 <paulin.trognon@gmail.com>
2013-11-19 17:39:24 +0900

    fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaulintrognonFr::Application.config.secret_token = '2416c69bb435f05018766d585f922334da61daf1608331ae82acbf958e3841e8c76aee8f4a13400d1e49736cd42ec348c96db60ea71b0734de99aa1c0ad4dfae'
