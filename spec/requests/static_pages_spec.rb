069fcd50633c3c4b239a950f7b9d3a557273bc96
OGRE7 <paulin.trognon@gmail.com>
2013-11-21 11:49:57 +0900

    refactoring static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,34 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe 'Static pages' do
 
-	describe "AboutMe page" do
+  let(:base_page_title) { 'paulintrognon.fr' }
 
-		it "should have the content 'About Me'" do
+	describe 'AboutMe page' do
+
+		it 'should have the content "About Me"' do
 			visit '/static_pages/aboutMe'
 			page.should have_selector('h1', :text => 'About Me')
 		end
 
-		it "should have the right title" do
+		it 'should have the right title' do
 			visit '/static_pages/aboutMe'
 			page.should have_selector('title',
-									  :text => " | About Me")
+									  :text => "#{base_page_title} | About Me")
 		end
 	end
 
-	describe "AboutWebsite page" do
+	describe 'AboutWebsite page' do
 
-		it "should have the content 'About This Website'" do
+		it 'should have the content "About This Website"' do
 			visit '/static_pages/aboutWebsite'
 			page.should have_selector('h1', :text => 'About This Website')
 		end
 
-		it "should have the right title" do
+		it 'should have the right title' do
 			visit '/static_pages/aboutWebsite'
 			page.should have_selector('title',
-									  :text => " | About This Website")
+									  :text => "#{base_page_title} | About This Website")
 		end
 	end
 end
