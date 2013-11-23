de3571fd1ea2ba8afa6bba4973de43c920b277c2
OGRE7 <paulin.trognon@gmail.com>
2013-11-23 14:52:36 +0900

    adding title helper to generate page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return the page title on a per-page basis
+  def full_page_title(page_title)
+    base_title = 'paulintrognon.fr'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
